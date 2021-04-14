f38d29be45324772ddd6bdb18474443ef674c87b
Wojtek <wojtas5127>
2021-04-14 10:27:19 +0200

    zadania nieskończone

---
+++ main.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+#zad1
+
+# a = np.arange(4,81,4)
+# print(a)
+
+#zad2
+
+# a = np.arange(5,dtype="float")
+# print(a)
+#
+# b = a.astype('int64')
+# print(b)
+
+#zad3
+
+# def tab(n):
+#     row = n * n
+#     return np.arange(1,row+1)
+#
+# print(tab(3))
+
+#zad4
+
+# def fun(l1,l2):
+#     return np.logspace(1,l2,num=l2,base=l1,dtype='int64')
+#
+# print(fun(2,4))
+
+#zad5
+
+# x = 4
+# wek = np.arange(x-1,-1,-1)
+#
+# mat_diag = np.diag([a for a in wek])
+#
+# print(wek)
+# print(mat_diag)
+
+#zad6
+
+
